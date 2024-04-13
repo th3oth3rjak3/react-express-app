58e2c11abe7ce35c37826af95d93ba4065ef3da0
Jake Hathaway <jake.d.hathaway@gmail.com>
2024-04-12 21:44:40 -0700

    create full stack shell app.

---
+++ client/src/App.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [apiResponse, setApiResponse] = useState("");
+
+  const getApiResponse = () =>
+    fetch("http://localhost:5000/api")
+      .then(res => res.json())
+      .then(body => setApiResponse(body.message));
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <button onClick={_ => getApiResponse()}>Call API</button>
+      <p>{apiResponse}</p>
+    </>
+  )
+}
+
+export default App
